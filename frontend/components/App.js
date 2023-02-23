8166ffb5e45430514564acfbd733b8c99ea9a589
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-02-22 19:35:28 -0500

    getting started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,20 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+      <div id='error'>Error: No Error Here</div>
+      <div id='todos'>
+        <h2>Todos:</h2>
+        <div>Walk the Dog</div>
+        <div>Learn React</div>
+        </div>
+        <form id='todoForm'>
+          <input type='text' placeholder='Type todo'></input>
+          <input type='submit'></input>
+          <button>Clear Complete</button>
+        </form>
+      </div>
+    )
   }
 }
